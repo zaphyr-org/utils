37d4cb4a1051ceaaf41812e00f58ab515fa8c3be
merloxx <hello@merloxx.it>
2023-04-09 11:38:17 +0200

    Changed: Updated Country.php to PHP 8.1 and added new functionality
    - Updated Contry.php to PHP 8.1
    - Added ISO codes as country array key
    - Added getNameByIsoCode method
    - Added getAllCountriesAsJsonString method

--- tests/CountryTest.php
+++ tests/CountryTest.php
@@ -10,12 +10,35 @@ use Zaphyr\Utils\Country;
 class CountryTest extends TestCase
 {
     /* -------------------------------------------------
+     * GET NAME BY ISO CODE
+     * -------------------------------------------------
+     */
+    public function testGetNameByIsoCode(): void
+    {
+        self::assertSame('Germany', Country::getNameByIsoCode('DE'));
+    }
+    public function testGetNameByIsoCodeReturnsNullOnInvalidIsoCode(): void
+    {
+        self::assertNull(Country::getNameByIsoCode('INVALID'));
+    }
+
+    /* -------------------------------------------------
      * GET ALL COUNTRIES
      * -------------------------------------------------
      */
 
     public function testGetAllCountries(): void
     {
-        self::assertSame(count(Country::getAllCountries()), 242);
+        self::assertSame(count(Country::getAllCountries()), 239);
+    }
+
+    /* -------------------------------------------------
+     * GET ALL COUNTRIES AS JSON STRING
+     * -------------------------------------------------
+     */
+
+    public function  testGetAllCountriesAsJsonString(): void
+    {
+        self::assertIsString(Country::getAllCountriesAsJsonString());
     }
 }
